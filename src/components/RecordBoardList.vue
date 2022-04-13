216de9a3a007ec6badd56ed78f3d1b7c50fb6d24
eric2788 <tsukiko852@gmail.com>
2022-04-13 22:56:45 +0800

    fix the ui of record board list

--- src/components/RecordBoardList.vue
+++ src/components/RecordBoardList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list theme="light" class="el-border" three-line>
+  <v-list theme="light" class="el-border" three-line min-height="400">
     <template v-if="records && records.length > 0">
       <template v-for="(item, index) in records" :key="index">
         <v-list-item
@@ -11,7 +11,7 @@
       </template>
     </template>
     <template v-else>
-        <v-list-item align=center justify=center title="没有记录" height="400" />
+        <v-list-item align=center justify=center title="没有记录" />
     </template>
   </v-list>
 </template>
