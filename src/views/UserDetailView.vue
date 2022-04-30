1c570cb84fdaa24c5b055970d0c9902bf524994e
eric2788 <tsukiko852@gmail.com>
2022-04-30 16:45:52 +0800

    user detail view 新增錯誤訊息返回

--- src/views/UserDetailView.vue
+++ src/views/UserDetailView.vue
@@ -139,6 +139,7 @@ export default {
         })
         .catch((err) => {
           console.error(err);
+          this.error = err?.response?.data?.message ?? err.statusText
           this.$emit("error",{ msg: "加载用户资讯时错误: ", err});
         })
         .finally(() => (this.loading = false));
